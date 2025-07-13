83cce7548b7b281e582c5cd0cc8ab33c5db2da70
adesh003 <theadeshkumar03@gmail.com>
2025-07-13 10:14:47 +0530

    login page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -17,7 +17,7 @@ exports.getOverview= catchAsync(async(req,res,next)=>{
    })})
 
 
-exports.getTour=catchAsync(async(req,res)=>{
+exports.getTour=catchAsync(async(req,res,next)=>{
    // 1) get the data , for the requested tour( include review , tour guide , )
 
 const tour = await Tour.findOne({ slug: req.params.slug })
@@ -36,5 +36,12 @@ const tour = await Tour.findOne({ slug: req.params.slug })
     title:`${tour.name} Tour`,
     tour
    })})
+
+   exports.getLoginForm = (req, res)=>{
+      res.status(200).render('login', {
+         title:'Log into your account'
+      })
+
+   }
 
 
